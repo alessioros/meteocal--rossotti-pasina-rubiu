4e9ee78efe88bab16932753e02e39a727051fc76
Simone Rubiu <simone.rubiu91@gmail.com>
2014-12-23 14:59:43 +0100

    entità

---
+++ MeteoCal/src/main/java/it/polimi/meteocal/business/entity/Notification.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.meteocal.business.entity;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Simone
+ */
+@Entity
+@Table(name = "notification")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Notification.findAll", query = "SELECT n FROM Notification n")})
+public class Notification implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "idNotification")
+    private Integer idNotification;
+    @Size(max = 45)
+    @Column(name = "Description")
+    private String description;
+    @JoinColumn(name = "idEvent", referencedColumnName = "idEvent")
+    @ManyToOne
+    private Event idEvent;
+    @OneToMany(mappedBy = "idNotification")
+    private Collection<Invitation> invitationCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "notification")
+    private Collection<Usernotification> usernotificationCollection;
+
+    public Notification() {
+    }
+
+    public Notification(Integer idNotification) {
+        this.idNotification = idNotification;
+    }
+
+    public Integer getIdNotification() {
+        return idNotification;
+    }
+
+    public void setIdNotification(Integer idNotification) {
+        this.idNotification = idNotification;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Event getIdEvent() {
+        return idEvent;
+    }
+
+    public void setIdEvent(Event idEvent) {
+        this.idEvent = idEvent;
+    }
+
+    @XmlTransient
+    public Collection<Invitation> getInvitationCollection() {
+        return invitationCollection;
+    }
+
+    public void setInvitationCollection(Collection<Invitation> invitationCollection) {
+        this.invitationCollection = invitationCollection;
+    }
+
+    @XmlTransient
+    public Collection<Usernotification> getUsernotificationCollection() {
+        return usernotificationCollection;
+    }
+
+    public void setUsernotificationCollection(Collection<Usernotification> usernotificationCollection) {
+        this.usernotificationCollection = usernotificationCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idNotification != null ? idNotification.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Notification)) {
+            return false;
+        }
+        Notification other = (Notification) object;
+        if ((this.idNotification == null && other.idNotification != null) || (this.idNotification != null && !this.idNotification.equals(other.idNotification))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "it.polimi.meteocal.business.entity.Notification[ idNotification=" + idNotification + " ]";
+    }
+    
+}
