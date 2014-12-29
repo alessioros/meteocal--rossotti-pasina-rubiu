713c1bac3c0378c6733d4649bf8a43891dab1004
matteopasina <matteopasina@gmail.com>
2014-12-29 02:32:45 +0100

    cancellata cosa che avevo messo inutile nel pom

--- MeteoCal/src/main/java/it/polimi/meteocal/business/control/SendEmails.java
+++ MeteoCal/src/main/java/it/polimi/meteocal/business/control/SendEmails.java
@@ -28,29 +28,20 @@ public class SendEmails {
 
         public void generateAndSendEmail(String to) throws AddressException, MessagingException {
  
-        //Step1
-        System.out.println("\n 1st ===> setup Mail Server Properties..");
         mailServerProperties = System.getProperties();
         mailServerProperties.put("mail.smtp.port", "587");
         mailServerProperties.put("mail.smtp.auth", "true");
-      //  mailServerProperties.put("mail.smtp.host", "smtp.gmail.com");
         mailServerProperties.put("mail.smtp.starttls.enable", "true");
-        System.out.println("Mail Server Properties have been setup successfully..");
- 
-        //Step2
-        System.out.println("\n\n 2nd ===> get Mail Session..");
+
         getMailSession = Session.getDefaultInstance(mailServerProperties, null);
         generateMailMessage = new MimeMessage(getMailSession);
         generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
         generateMailMessage.setSubject("Confirm Registration");
         String emailBody = "Test email " + "<br><br> Regards, <br>MeteoCal Admin";
         generateMailMessage.setContent(emailBody, "text/html");
-        System.out.println("Mail Session has been created successfully..");
- 
-        //Step3
-        System.out.println("\n\n 3rd ===> Get Session and Send mail");
+
         Transport transport = getMailSession.getTransport("smtp");
-        // Enter your correct gmail UserID and Password (XXXarpitshah@gmail.com)
+
         transport.connect("smtp.gmail.com", "meteocalpolimi@gmail.com", "bubbole123");
         transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
         transport.close();
