4af52fd982cb714b099876ee8c6602ff17a45231
alessioros <alessiorossotti@gmail.com>
2014-12-29 17:05:47 +0100

    personal data funzionante

--- MeteoCal/src/main/java/it/polimi/meteocal/business/control/ManagePersonalData.java
+++ MeteoCal/src/main/java/it/polimi/meteocal/business/control/ManagePersonalData.java
@@ -5,8 +5,13 @@
  */
 package it.polimi.meteocal.business.control;
 
+import it.polimi.meteocal.business.beans.UserBean;
 import it.polimi.meteocal.business.entity.User;
+import java.security.Principal;
+import java.sql.*;
 import javax.ejb.Stateless;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -19,6 +24,9 @@ public class ManagePersonalData {
     
     @PersistenceContext
     EntityManager em;
+    
+    @Inject
+    Principal principal;
     
     private User user;
     
@@ -37,10 +45,43 @@ public class ManagePersonalData {
     public User getData(){
         
         this.user=new User();
-        user.setUsername("#provausername");
-        user.setName("alessio");
-        user.setSurname("provaros");
-        user.setEmail("alessiorossotti@gmail.com");
+        principal = FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
+        String username = principal.getName();
+        PreparedStatement ps = null;
+        Connection con = null;
+        ResultSet rs = null;
+        
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/meteocal", "root", "root");
+            String sql = "select * from user where username='"+username+"'";
+            ps= con.prepareStatement(sql); 
+            rs= ps.executeQuery(); 
+            
+            while(rs.next()){
+                user.setUsername(username);
+                user.setName(rs.getString("Name"));
+                user.setSurname(rs.getString("Surname"));
+                user.setEmail(rs.getString("Email"));
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                con.close();
+                ps.close();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
         
         return this.user;
     }
