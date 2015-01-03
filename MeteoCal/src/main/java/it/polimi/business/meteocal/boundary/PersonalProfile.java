1a8d101bd4abf44e3187d2bd159515b0e9a05766
matteopasina <matteopasina@gmail.com>
2015-01-03 12:52:18 +0100

    personal profile mostra i dati dell'utente

--- MeteoCal/src/main/java/it/polimi/business/meteocal/boundary/PersonalProfile.java
+++ MeteoCal/src/main/java/it/polimi/business/meteocal/boundary/PersonalProfile.java
@@ -6,9 +6,11 @@
 package it.polimi.business.meteocal.boundary;
 
 import it.polimi.meteocal.business.control.ManagePersonalData;
+import it.polimi.meteocal.business.control.RegisterValidation;
 import it.polimi.meteocal.business.entity.User;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
@@ -18,15 +20,18 @@ import javax.inject.Named;
 @Named
 @RequestScoped
 public class PersonalProfile {
-    
+
     @EJB
     private ManagePersonalData mpd;
-    
+
+    @Inject
+    RegisterValidation rv;
+
     private User user;
     private String contact;
     private String message;
     private String confPassword;
-    
+
     public String getConfPassword() {
         return this.confPassword;
     }
@@ -34,7 +39,7 @@ public class PersonalProfile {
     public void setConfPassword(String confPassword) {
         this.confPassword = confPassword;
     }
-    
+
     public String getMessage() {
         return this.message;
     }
@@ -42,53 +47,51 @@ public class PersonalProfile {
     public void setMessage(String message) {
         this.message = message;
     }
-    
-    public void setUser(User user){
-        this.user=user;
+
+    public void setUser(User user) {
+        this.user = user;
     }
-    
-    public User getUser(){
-    
-        if(this.user==null){
-            this.user=new User();
+
+    public User getUser() {
+
+        if (this.user == null) {
+            this.user = new User();
         }
         return user;
     }
-    
-    public void setContact(String contact){
-        this.contact=contact;
+
+    public void setContact(String contact) {
+        this.contact = contact;
     }
-    
-    public String getContact(){
-    
+
+    public String getContact() {
+
         return contact;
     }
-    public void showPersonalData(){
-        
-        this.mpd=new ManagePersonalData();
-        user=mpd.getData();
+
+    public User showPersonalData() {
+        return user = rv.getLoggedUser();
     }
-    
-    public String submitChangeData(){
-        
-        this.mpd=new ManagePersonalData();
-        boolean success=false;
-        
-        success=mpd.changeData(user);
-        
-        if(success){
-            this.message="Data succesfully updated!";
+
+    public String submitChangeData() {
+
+        this.mpd = new ManagePersonalData();
+        boolean success = false;
+
+        success = mpd.changeData(user);
+
+        if (success) {
+            this.message = "Data succesfully updated!";
+        } else {
+            this.message = "Something wrong!";
         }
-        else{
-            this.message="Something wrong!";
-        }
-        
+
         return "";
-        
+
     }
-    
-    public void submitAddUser(){
-        
-       mpd.addUser(contact);
+
+    public void submitAddUser() {
+
+        mpd.addUser(contact);
     }
 }
