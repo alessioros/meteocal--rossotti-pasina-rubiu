ef0165459ab20c60687fb6e7834878c44bccf9ac
alessioros <alessiorossotti@gmail.com>
2015-01-04 17:37:09 +0100

    aggiunte date nella creazione evento

--- MeteoCal/src/main/java/it/polimi/business/meteocal/boundary/EventDetails.java
+++ MeteoCal/src/main/java/it/polimi/business/meteocal/boundary/EventDetails.java
@@ -3,6 +3,11 @@ package it.polimi.business.meteocal.boundary;
 import it.polimi.meteocal.business.control.ManageEvent;
 import it.polimi.meteocal.business.entity.Event;
 import it.polimi.meteocal.business.entity.Location;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
@@ -17,6 +22,37 @@ public class EventDetails {
     private Event event;
     private Location loc;
     private String message;
+    private SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy : HH:mm");
+    private String startDateStr;
+    private String endDateStr;
+    
+    
+    public Date dateConverter(String input){
+        
+        Date date=null;
+        try {
+            date = formatter.parse(input);
+        } catch (ParseException ex) {
+            Logger.getLogger(EventDetails.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return date;
+    }
+    public String getStartDateStr() {
+        return this.startDateStr;
+    }
+
+    public void setStartDateStr(String startDateStr) {
+        this.startDateStr = startDateStr;
+    }
+    
+    public String getEndDateStr() {
+        return this.endDateStr;
+    }
+
+    public void setEndDateStr(String startDateStr) {
+        this.endDateStr = startDateStr;
+    }
     
     public String getMessage() {
         return this.message;
@@ -48,7 +84,12 @@ public class EventDetails {
         this.event = event;
     }
 
-    public String create() {
+    public String create() {		
+        
+        
+        event.setStartTime(dateConverter(startDateStr));
+	event.setEndTime(dateConverter(endDateStr));
+
         me.createEvent(event,loc);
         
         message="Event Created!";
