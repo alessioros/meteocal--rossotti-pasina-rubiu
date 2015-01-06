41ccc9703e31f4f6b523b2891985708662a8c1fc
alessioros <alessiorossotti@gmail.com>
2015-01-06 19:24:04 +0100

    aggiunta verifica utente verificato

--- MeteoCal/src/main/java/it/polimi/meteocal/business/beans/LoginBean.java
+++ MeteoCal/src/main/java/it/polimi/meteocal/business/beans/LoginBean.java
@@ -4,12 +4,20 @@
  * and open the template in the editor.
  */
 package it.polimi.meteocal.business.beans;
+import it.polimi.meteocal.business.entity.User;
+import java.util.List;
+import javax.annotation.Resource;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 
 /**
  *
@@ -25,6 +33,9 @@ public class LoginBean {
     private String password;
     private String username;
     private String message;
+    
+    @PersistenceContext
+    EntityManager em;
 
     public LoginBean() {
     }
@@ -55,17 +66,38 @@ public class LoginBean {
 
     public String login() {
         
+        Query query;
+        try {
+            
+            query = em.createQuery("select u from User u where u.username=:un and u.verified like :v");
+            query.setParameter("un", username);
+            query.setParameter("v", true);
+            List check=query.getResultList();
+            
+            if (check.isEmpty()) {
+                
+                message="User not verified!";
+                return "";
+            
+            }
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+        
         FacesContext context = FacesContext.getCurrentInstance();
         HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
         try {
             request.login(this.username, this.password);
             return "/loggeduser/calendar.xhtml?faces-redirect=true";
         } catch (ServletException e) {
+            
             context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Login Failed","Login Failed"));
             //logger.log(Level.SEVERE,"Login Failed");
             message="Login Failed!";
             return "";
-        }
+        } 
+  
     }
     public String logout() {
         FacesContext context = FacesContext.getCurrentInstance();
