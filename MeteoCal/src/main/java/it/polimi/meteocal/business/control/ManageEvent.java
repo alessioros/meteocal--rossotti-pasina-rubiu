32db73401cddc30201c3f50e49ebc15cb980464d
alessioros <alessiorossotti@gmail.com>
2015-01-07 14:40:51 +0100

    aggiunto evento alla event collection

--- MeteoCal/src/main/java/it/polimi/meteocal/business/control/ManageEvent.java
+++ MeteoCal/src/main/java/it/polimi/meteocal/business/control/ManageEvent.java
@@ -2,6 +2,7 @@ package it.polimi.meteocal.business.control;
 
 import it.polimi.meteocal.business.entity.Event;
 import it.polimi.meteocal.business.entity.Location;
+import java.util.Collection;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -18,10 +19,16 @@ public class ManageEvent {
     
   
     public void createEvent(Event event,Location loc) {
+        
+        Collection<Event> eventi;
+        
         em.persist(loc);
         event.setIdLocation(loc);
         event.setIdOrganizer(rv.getLoggedUser());
         em.persist(event);
+        eventi=rv.getLoggedUser().getEventCollection();
+        eventi.add(event);
+        rv.getLoggedUser().setEventCollection(eventi);
     }
     
     public void deleteEvent(){
