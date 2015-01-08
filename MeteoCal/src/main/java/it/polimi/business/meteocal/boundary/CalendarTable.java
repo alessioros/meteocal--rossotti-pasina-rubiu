c5d6c4999b57b0bb920b60f93122c5184a024c6b
Simone Rubiu <simone.rubiu91@gmail.com>
2015-01-08 11:48:19 +0100

    sistemato calendar

--- MeteoCal/src/main/java/it/polimi/business/meteocal/boundary/CalendarTable.java
+++ MeteoCal/src/main/java/it/polimi/business/meteocal/boundary/CalendarTable.java
@@ -5,11 +5,14 @@
  */
 package it.polimi.business.meteocal.boundary;
 
+import it.polimi.meteocal.business.control.Day;
 import it.polimi.meteocal.business.control.ManageCalendar;
+import it.polimi.meteocal.business.control.Week;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import javax.enterprise.context.RequestScoped;
@@ -25,120 +28,85 @@ import javax.servlet.http.HttpSession;
  */
 @Named
 @RequestScoped
-public class CalendarTable{
-    private static Date today=new Date();
+public class CalendarTable{    
     private Date date;
-    
-    private boolean set =false;
+    private List<Week> cal = new ArrayList();    
     private int actual=13;
     
     @Inject 
     ManageCalendar mc;
     
-    public CalendarTable() {       
-        Principal principal;
-        HttpSession session;
-        FacesContext context = FacesContext.getCurrentInstance();
-        principal = FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
-        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
-        session = request.getSession(); 
-        session.setAttribute("id", principal.getName());
-        date=this.getSessionDate();
-        if(date == null)
-        {
-            date=new Date();
-            date.setDate(1);
-            session.setAttribute("cal", date);
-        }
-        
-    }
-    private Date getSessionDate(){
-        //Principal principal;
-        HttpSession session;
-        FacesContext context = FacesContext.getCurrentInstance();
-        //principal = FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
-        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
-        session = request.getSession();        
-        date=(Date)session.getAttribute("cal");
-        return date;
-    }  
-    public Date getDate() {
-        date=this.getSessionDate();
+    public CalendarTable() {                            
+        this.getSessionDate();
         if(date == null){
             date=new Date();
             date.setDate(1);
+            this.setSessionDate();
+        }       
+    }
+    private void getSessionDate(){
+        HttpSession session;
+        FacesContext context = FacesContext.getCurrentInstance();    
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        session = request.getSession();        
+        date=(Date)session.getAttribute("cal");
+    }  
+    private void setSessionDate(){
+        HttpSession session;
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        session = request.getSession();
+        session.setAttribute("cal", date);                  
+    }
+    public Date getDate() {       
+        if(date == null){
+            date=new Date();
+            date.setDate(1);
+            this.setSessionDate();
         }
         return date;
     }   
-    public void today(){
-        date=this.getSessionDate();
-        date=new Date();
+    public void today(){        
+        date=new Date();  
+        this.setSessionDate();        
     }
     public String getDayNum(){
-        date=this.getSessionDate();
         return new SimpleDateFormat("d",Locale.US).format(this.getDate());
     } 
     public String getDayStr(){
-        date=this.getSessionDate();
         return new SimpleDateFormat("E",Locale.US).format(this.getDate());
     }
     public String getMonthStr(){
-        date=this.getSessionDate();
         return new SimpleDateFormat("MMMM",Locale.US).format(this.getDate());
     }
     public String getMonthNum(){
-        date=this.getSessionDate();
         return new SimpleDateFormat("M",Locale.US).format(this.getDate());
     }
     public String getYear(){
-        date=this.getSessionDate();
         return new SimpleDateFormat("y",Locale.US).format(this.getDate());
     }
     public String getHour(){
-        date=this.getSessionDate();
         return new SimpleDateFormat("H",Locale.US).format(this.getDate());
     }
     public String getMinute(){
-        date=this.getSessionDate();
         return new SimpleDateFormat("m",Locale.US).format(this.getDate());
     }
+    public List<Week> getCal() {
+        return cal;
+    }
+    public void setCal(List<Week> cal) {
+        this.cal = cal;
+    }
     public void next(){
-        date=this.getSessionDate();
-            date.setDate(1);
-            date.setMonth(this.getDate().getMonth()+1);
-            set = false;
+        date.setDate(1);
+        date.setMonth(this.getDate().getMonth()+1);
     }
     public void prev(){
-        date=this.getSessionDate();
         date.setDate(1);
-        date.setMonth(this.getDate().getMonth()-1);
-        set = false;
+        date.setMonth(this.getDate().getMonth()-1);      
     }
-    public String displayNumDay(int pos){
-        date=this.getSessionDate();
-         String result="";
-         if(!set)
-            if(pos==1){
-                actual=date.getMonth();
-                date.setDate(1);
-                if(date.getDay()==1)
-                    date.setDate(date.getDate()-7);
-                else
-                    date.setDate(date.getDate()-((date.getDay()+6)%7));                  
-            }
-            if(pos%7==date.getDay()){
-                result=this.getDayNum();                    
-                if(pos<42)
-                    date.setDate(date.getDate()+1); 
-                else{
-                    date.setMonth(date.getMonth()-1);
-                    date.setDate(1);
-                    set=true;
-                }
-            }                
-         return result;   
-     }   
-    public String displayColDay(int pos){ 
+   
+    public String weekDay(int pos){ 
         if(pos%7==0&&date.getMonth()==actual)
             return "dayNumbHoly";
         else
@@ -150,19 +118,44 @@ public class CalendarTable{
                 else
                     return "dayNumbOld";
          }   
-    public String displayEventClass(){
+    public String eventClass(){
         if(mc.scheduleEvent(date))
             return "event";
         else
             return "empty";
     }
-    public String displayEventOutcome(){
+    public String eventOutcome(){
        /* if(mc.scheduleEvent(date))
             return "today_events?date=";
         else
             return "createEvent?date=";*/
         return "";
     }
+
     
     
+    public void createCal(){
+        //trovo il primo giorno da visualizzare                         
+        actual=date.getMonth();
+        date.setDate(1);
+        if(date.getDay()==1)
+            date.setDate(date.getDate()-7);
+        else
+            date.setDate(date.getDate()-((date.getDay()+6)%7));  
+        
+        for(int j=0;j<6;j++){   
+            Week w = new Week();
+            for(int i =1;i<8;i++){
+                Day d = new Day(date,this.eventOutcome(),this.eventClass(),this.weekDay(i));
+                w.getWeek().add(d);
+                date.setDate(date.getDate()+1);                 
+            }
+            cal.add(w);
+        }
+        date.setMonth(date.getMonth()-1);
+        date.setDate(1);
+        
+            
+        
+    }
 }
---
+++ MeteoCal/src/main/java/it/polimi/meteocal/business/control/Day.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.meteocal.business.control;
+
+import java.util.Date;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Simone
+ */
+@Named
+@RequestScoped
+public class Day {
+    private Date date;
+    private String eventOutcome;
+    private String eventClass;
+    private String weekDay;
+
+    public Day(Date date, String eventOutcome, String eventClass, String weekDay) {
+        this.date = (Date)date.clone();
+        this.eventOutcome = eventOutcome;
+        this.eventClass = eventClass;
+        this.weekDay = weekDay;
+    }
+    public Date getDate() {
+        return (Date)date.clone();
+    }
+
+    public void setDate(Date date) {
+        this.date =(Date) date.clone();
+    }
+
+    public String getEventOutcome() {
+        return eventOutcome;
+    }
+
+    public void setEventOutcome(String eventOutcome) {
+        this.eventOutcome = eventOutcome;
+    }
+
+    public String getEventClass() {
+        return eventClass;
+    }
+
+    public void setEventClass(String eventClass) {
+        this.eventClass = eventClass;
+    }
+
+    public String getWeekDay() {
+        return weekDay;
+    }
+
+    public void setWeekDay(String weekDay) {
+        this.weekDay = weekDay;
+    }
+    
+    public int dayNum(){
+        return date.getDate();
+    }
+}
