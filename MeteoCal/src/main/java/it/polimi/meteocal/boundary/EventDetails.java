9e25a79d03ebc6c313683dd417d7ea70d049a96a
matteopasina <matteopasina@gmail.com>
2015-01-15 15:28:10 +0100

    create event con lat e long e qualcosa sul forecast

--- MeteoCal/src/main/java/it/polimi/meteocal/boundary/EventDetails.java
+++ MeteoCal/src/main/java/it/polimi/meteocal/boundary/EventDetails.java
@@ -3,6 +3,7 @@ package it.polimi.meteocal.boundary;
 import it.polimi.meteocal.control.CheckFields;
 import it.polimi.meteocal.control.ManageEvent;
 import it.polimi.meteocal.control.RegisterValidation;
+import it.polimi.meteocal.control.YahooQueries;
 import it.polimi.meteocal.entity.Event;
 import it.polimi.meteocal.entity.Location;
 import java.text.SimpleDateFormat;
@@ -10,45 +11,119 @@ import java.util.Date;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import org.json.JSONObject;
 
 @Named
 @RequestScoped
 public class EventDetails {
-    
+
+    @EJB
+    YahooQueries yq;
+
     @EJB
     private ManageEvent me;
-    
+
     @EJB
     private CheckFields cf;
-    
+
     @EJB
     private RegisterValidation rv;
 
     private Event event;
-    private Location loc;
+    private Location loc=new Location();
     private String message;
     private SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy   HH:mm");
     private long startDateLong;
     private long endDateLong;
     private Date startDate;
     private Date endDate;
-    private String lat;
-    private String lon;
-    
-    public void setLat(String lat) {
-        this.lat = lat;
+    private String state;
+    private String city;
+    private String address;
+    private String query;
+
+    public void dateConverter() {
+        this.startDate = new Date(this.startDateLong);
+        this.endDate = new Date(this.endDateLong + 3600000);
+        //this.startDate.setTime(this.startDateLong);
+        //this.endDate.setTime(this.endDateLong);               
     }
 
-    public void setLon(String lon) {
-        this.lon = lon;
-    }
-    
-    public String getLat() {
-        return lat;
+    public String create() {
+
+        event.setIdOrganizer(rv.getLoggedUser());
+        event.setStartTime(startDate);
+        event.setEndTime(endDate);
+
+        query = "select * from geo.placefinder where text=\"" + address + "," + city + "," + state+"\"";
+        try {
+            JSONObject json = yq.yahooRestQuery(query);
+
+            // Ci faccio quel che devo.
+            // Ad esempio, stampo le previsioni per tutti i giorni:
+            JSONObject jsonQuery = json.getJSONObject("query");
+            JSONObject queryResults = jsonQuery.getJSONObject("results");
+            JSONObject pl = queryResults.getJSONObject("Result");
+            System.out.println(Float.parseFloat(pl.getString("latitude")));
+            loc.setLatitude(Float.parseFloat(pl.getString("latitude")));
+            loc.setLongitude(Float.parseFloat(pl.getString("longitude")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        loc.setAddress(address);
+        loc.setCity(city);
+        loc.setState(state);
+
+        if (!cf.checkDateTimes(event.getStartTime(), event.getEndTime())) {
+
+            message = "Error! Start time must be after End Time";
+            return "";
+        } else if (!cf.checkCoordinates(loc.getLatitude(), loc.getLongitude())) {
+
+            message = "Error! Invalid coordinates";
+            return "";
+        } else {
+
+            me.createEvent(event, loc);
+
+            message = "Event Created!";
+
+            return "/loggeduser/invitePeople.xhtml?faces-redirect=true&event=" + event.getIdEvent();
+
+        }
+
     }
 
-    public String getLon() {
-        return lon;
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
     }
 
     public Date getStartDate() {
@@ -66,17 +141,15 @@ public class EventDetails {
     public void setEndDate(Date endDate) {
         this.endDate = endDate;
     }
-    
-    
-    
+
     public long getStartDateLong() {
         return this.startDateLong;
     }
 
     public void setStartDateLong(long startDateLong) {
-         this.startDateLong = startDateLong;
+        this.startDateLong = startDateLong;
     }
-    
+
     public long getEndDateLong() {
         return this.endDateLong;
     }
@@ -84,7 +157,7 @@ public class EventDetails {
     public void setEndDateLong(long startDateLong) {
         this.endDateLong = startDateLong;
     }
-    
+
     public String getMessage() {
         return this.message;
     }
@@ -92,7 +165,7 @@ public class EventDetails {
     public void setMessage(String message) {
         this.message = message;
     }
-    
+
     public Location getLoc() {
         if (loc == null) {
             loc = new Location();
@@ -103,7 +176,7 @@ public class EventDetails {
     public void setLoc(Location loc) {
         this.loc = loc;
     }
-    
+
     public Event getEvent() {
         if (event == null) {
             event = new Event();
@@ -115,41 +188,5 @@ public class EventDetails {
         this.event = event;
     }
     
-    public void dateConverter(){
-        this.startDate = new Date(this.startDateLong);
-        this.endDate = new Date(this.endDateLong+3600000);
-        //this.startDate.setTime(this.startDateLong);
-        //this.endDate.setTime(this.endDateLong);               
-    }
-    
-    public String create() {		
-        
-        event.setIdOrganizer(rv.getLoggedUser());
-        event.setStartTime(startDate);
-	event.setEndTime(endDate);
-        loc.setLatitude(Float.parseFloat(lat));
-        loc.setLongitude(Float.parseFloat(lon));
-        
-        if(!cf.checkDateTimes(event.getStartTime(), event.getEndTime())){
-        
-            message="Error! Start time must be after End Time";
-            return "";
-        }
-        else if(!cf.checkCoordinates(loc.getLatitude(),loc.getLongitude())){
-            
-            message="Error! Invalid coordinates";
-            return "";
-        }
-        else{
-            
-            me.createEvent(event,loc);
-        
-            message="Event Created!";
 
-            return "/loggeduser/invitePeople.xhtml?faces-redirect=true&event="+event.getIdEvent();
-            
-        }
-        
-    }
-    
 }
--- MeteoCal/src/main/java/it/polimi/meteocal/control/ManageForecast.java
+++ MeteoCal/src/main/java/it/polimi/meteocal/control/ManageForecast.java
@@ -5,12 +5,12 @@
  */
 package it.polimi.meteocal.control;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
+import it.polimi.meteocal.entity.Forecast;
+import it.polimi.meteocal.entity.Location;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.Schedule;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.persistence.EntityManager;
@@ -26,59 +26,64 @@ import org.json.JSONObject;
 @ViewScoped
 public class ManageForecast {
 
-    private URL weatherurl;
-    private String urlPrefix = "https://query.yahooapis.com/v1/public/yql?q=";
-    private String urlSuffix = "&format=json&diagnostics=true&callback=";
     private String urlQuery;
     private String woeid;
-    private String location;
     private String cityHome;
     private String tempHome;
-    
+    private List<Location> place;
+    private Forecast forecast;
+
+    @EJB
+    YahooQueries yq;
+
     @PersistenceContext
     EntityManager em;
 
-    //  @PostConstruct
-    //  @Schedule(hour = "*/1", minute = "0", second = "0", persistent = false)
-  /*  public void updateForecast() {
-     try {
-     urlPrefix = "https://query.yahooapis.com/v1/public/yql?q=";
-     urlSuffix = "&format=json&diagnostics=true&callback=";
-     // Query da eseguire su Yahoo Weather
-     urlQuery = "select * from weather.forecast where woeid=2502265";
+    @PostConstruct
+    @Schedule(hour = "*/1", minute = "0", second = "0", persistent = false)
+    public void updateForecast() {
+        try {
+            place = em.createQuery("select l from Location l").getResultList();
+            for (Location location : place) {
+                
+                woeid=woeidOfLocation(location.getAddress()+", "+location.getCity()+", "+location.getState());
+               
+                // Query da eseguire su Yahoo Weather
+                urlQuery = "select * from weather.forecast where woeid="+woeid;
 
-     // Costruisco il JSON
-     JSONObject json = yahooRestQuery(urlQuery);
+                // Costruisco il JSON
+                JSONObject json = yq.yahooRestQuery(urlQuery);
 
-     // Ci faccio quel che devo.
-     // Ad esempio, stampo le previsioni per tutti i giorni:
-     JSONObject jsonQuery = json.getJSONObject("query");
-     JSONObject queryResults = jsonQuery.getJSONObject("results");
-     JSONObject channel = queryResults.getJSONObject("channel");
-     JSONObject resItem = channel.getJSONObject("item");
-     JSONArray forecasts = resItem.getJSONArray("forecast");
-     for (int i = 0; i < forecasts.length(); i++) {
-     JSONObject fc = forecasts.getJSONObject(i);
+            
+                // stampo le previsioni per tutti i giorni:
+                JSONObject jsonQuery = json.getJSONObject("query");
+                JSONObject queryResults = jsonQuery.getJSONObject("results");
+                JSONObject channel = queryResults.getJSONObject("channel");
+                JSONObject resItem = channel.getJSONObject("item");
+                JSONArray forecasts = resItem.getJSONArray("forecast");
+                for (int i = 0; i < forecasts.length(); i++) {
+                    JSONObject fc = forecasts.getJSONObject(i);
+                    forecast.setGeneral(fc.getString("text"));
+                    forecast.setMaxTemp(fc.getString("high"));
+                    forecast.setMinTemp(fc.getString("low"));
+                    forecast.setIdLocation(location);
+                    em.persist(forecast);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-     System.out.println("Data: " + fc.getString("date"));
-     System.out.println("Previsione: " + fc.getString("text"));
-     System.out.println("High: " + fc.getString("high"));
-     System.out.println("Low: " + fc.getString("low"));
-     System.out.println();
-     }
-     } catch (Exception e) {
-     e.printStackTrace();
-     }
+    }
 
-     }*/
-    public void getWoeidOfLocation() {
+    public String woeidOfLocation(String location) {
         try {
 
             // Query da eseguire su Yahoo Weather
             urlQuery = "select * from geo.placefinder where text=\"" + location + "\"";
 
             // Costruisco il JSON
-            JSONObject json = yahooRestQuery(urlQuery);
+            JSONObject json = yq.yahooRestQuery(urlQuery);
 
             // Ci faccio quel che devo.
             // Ad esempio, stampo le previsioni per tutti i giorni:
@@ -86,23 +91,25 @@ public class ManageForecast {
             JSONObject queryResults = jsonQuery.getJSONObject("results");
             JSONObject pl = queryResults.getJSONObject("Result");
 
-            woeid = pl.getString("woeid");
+            return pl.getString("woeid");
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return null;
 
     }
 
-    public void getForecast() {
+    public void forecast() {
+        String location;
+
         try {
-            getWoeidOfLocation();
+            // woeidOfLocation(location);
 
             urlQuery = "select * from weather.forecast where woeid=" + woeid;
 
             // Costruisco il JSON
-            JSONObject jsonWeather = yahooRestQuery(urlQuery);
+            JSONObject jsonWeather = yq.yahooRestQuery(urlQuery);
 
-  
             JSONObject jsonQueryWeather = jsonWeather.getJSONObject("query");
             JSONObject queryRes = jsonQueryWeather.getJSONObject("results");
             JSONObject channel = queryRes.getJSONObject("channel");
@@ -110,7 +117,7 @@ public class ManageForecast {
             JSONArray forecasts = resItem.getJSONArray("forecast");
             for (int i = 0; i < forecasts.length(); i++) {
                 JSONObject fc = forecasts.getJSONObject(i);
-                
+
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -121,17 +128,15 @@ public class ManageForecast {
     public void weatherHome() {
         try {
 
-            cityHome = "Milano";
+            cityHome = "New York";
 
-            location = cityHome;
-
-            getWoeidOfLocation();
+            woeid=woeidOfLocation(cityHome);
 
             urlQuery = "select * from weather.forecast where u=\"c\" and woeid=" + woeid;
 
             // Costruisco il JSON
-            JSONObject jsonWeather = yahooRestQuery(urlQuery);
-            
+            JSONObject jsonWeather = yq.yahooRestQuery(urlQuery);
+
             //Estraggo la temperatura dal JSON
             JSONObject jsonQueryWeather = jsonWeather.getJSONObject("query");
             JSONObject queryRes = jsonQueryWeather.getJSONObject("results");
@@ -144,67 +149,6 @@ public class ManageForecast {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-    
-    /**
-     * Receives a query, it asks yahooWeather service whit the query. It receives
-     * the data and rebuilds the JSON, it returns this.
-     * @param urlQuery
-     * @return well formed json object with query answer
-     */
-    public JSONObject yahooRestQuery(String urlQuery) {
-
-        try {
-
-            // Costruisco l'URL (avendo l'accortezza di fare l'encoding della query)    
-            weatherurl = new URL(urlPrefix + URLEncoder.encode(urlQuery, "UTF-8") + urlSuffix);
-
-            // Ottengo la connessione al servizio
-            URLConnection con = weatherurl.openConnection();
-
-            // Preparo lo StringBuilder che accoglierà il JSON
-            StringBuilder myJson = new StringBuilder();
-
-            // Eseguo la request e leggo il risultato
-            InputStream is = con.getInputStream();
-
-            // Essendo che ricevo un JSON (quindi una stringa) posso usare in Reader
-            InputStreamReader reader = new InputStreamReader(is);
-            try (BufferedReader br = new BufferedReader(reader)) {
-                String line = null;
-
-                // Leggo finchè ci sono dati da leggere
-                while ((line = br.readLine()) != null) {
-                    myJson.append(line);
-                }
-
-                // Ho finito, chiudo la connessione
-                br.close();
-            }
-
-            JSONObject json = new JSONObject(myJson.toString());
-            return json;
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public String getUrlPrefix() {
-        return urlPrefix;
-    }
-
-    public void setUrlPrefix(String urlPrefix) {
-        this.urlPrefix = urlPrefix;
-    }
-
-    public String getUrlSuffix() {
-        return urlSuffix;
-    }
-
-    public void setUrlSuffix(String urlSuffix) {
-        this.urlSuffix = urlSuffix;
     }
 
     public String getUrlQuery() {
@@ -221,14 +165,6 @@ public class ManageForecast {
 
     public void setWoeid(String woeid) {
         this.woeid = woeid;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
     }
 
     public String getCityHome() {
